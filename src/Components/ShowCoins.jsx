b6620f0933fc3b96d4730836cb6ee58c65c45da5
Fede <ferrelli.mazza@gmail.com>
2022-09-05 21:50:26 -0300

    Reorder fav logic

--- src/Components/ShowCoins.jsx
+++ src/Components/ShowCoins.jsx
@@ -1,155 +1,163 @@
-import React, {useState, useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {ImSpinner9} from 'react-icons/im';
-import {AiFillHeart, AiOutlineHeart} from 'react-icons/ai';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ImSpinner9 } from "react-icons/im";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { Fade } from "react-awesome-reveal";
 
-
-import db from '../assets/firebase';
+import db from "../assets/firebase";
 import { collection, doc, setDoc, getDocs } from "firebase/firestore";
 
+function ShowCoins({ data }) {
+  const [favourites, setFavourites] = useState([]);
+  const [checkFavourites, setCheckFavourites] = useState(false);
 
+  const [filter, setFilter] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
 
+  const [showLoading, setShowLoading] = useState(true);
 
+  // const [favourites, setFavourites] = useState([]);
 
-function ShowCoins( {data} ) {
+  useEffect(() => {
+    const downloadFavourites = async () => {
+      const querySnapshot = await getDocs(collection(db, "cities"));
+      querySnapshot.forEach((doc) => {
+        setFavourites(doc.data().favs);
+      });
+    };
+    downloadFavourites();
+  }, [checkFavourites]);
 
-    const [favourites, setFavourites] = useState([])
-    const [checkFavourites, setCheckFavourites ] = useState(false)
+  const uploadFavourites = async (arrFav) => {
+    await setDoc(doc(db, "cities", "LA"), {
+      favs: arrFav,
+    });
 
-    const [filter, setFilter] = useState("");
-    const [filteredData, setFilteredData] = useState([]);
+    setCheckFavourites(!checkFavourites);
+  };
 
-    const [showLoading, setShowLoading] = useState(true);
+  const addFavToUpload = (n) => {
+    let newFavs = [...favourites, n];
+    uploadFavourites(newFavs);
+  };
 
-   // const [favourites, setFavourites] = useState([]);
+  const deleteFavToUpload = (n) => {
+    let newFavs = [...favourites.filter((i) => i != n)];
+    uploadFavourites(newFavs);
+  };
 
-
-    useEffect(() => {
-        const downloadFavourites = async () =>{
-            const querySnapshot = await getDocs(collection(db, "cities"));
-        querySnapshot.forEach((doc) => {
-        
-          setFavourites(doc.data().favs);
-          
-        });
-        };
-        downloadFavourites();
-    }, [checkFavourites])
-
-    
-    const uploadFavourites = async (arrFav) => {
-
-        await setDoc(doc(db, "cities", "LA"), {
-            favs : arrFav
-          });
-    }
-
-
-    const addFavToUpload = (n) => {
-        let newFavs = [...favourites, n];
-        uploadFavourites(newFavs);
-        setCheckFavourites(!checkFavourites)
-    }
-
-    const deleteFavToUpload = (n) => {
-        let newFavs = [...favourites.filter(i=>i!=n)];
-        uploadFavourites(newFavs);
-        setCheckFavourites(!checkFavourites)
-    }
-
-   
-  { data &&
-  
-    useEffect(() => {
-        
-       
-      
-        const getFilteredData = () =>{
-
-            const filterAux = data.filter((e)=>
-            e.name.toLowerCase().includes(filter.toLowerCase())
-            ||
-            e.symbol.toLowerCase().includes(filter.toLowerCase())
-            ||
-            e.id.toLowerCase().includes(filter.toLowerCase())
-            ) 
-            setFilteredData(filterAux);
-            setShowLoading(false)
+  {
+    data &&
+      useEffect(() => {
+        const getFilteredData = () => {
+          const filterAux = data.filter(
+            (e) =>
+              e.name.toLowerCase().includes(filter.toLowerCase()) ||
+              e.symbol.toLowerCase().includes(filter.toLowerCase()) ||
+              e.id.toLowerCase().includes(filter.toLowerCase())
+          );
+          setFilteredData(filterAux);
+          setShowLoading(false);
         };
 
-        getFilteredData()
-    }, [filter])
-}
+        getFilteredData();
+      }, [filter, checkFavourites]);
+  }
 
+  const navigate = useNavigate();
 
+  const setCoinToBeDetailed = (coin) => {
+    sessionStorage.setItem("token", JSON.stringify(coin));
+    navigate("/detailedCoin");
+    console.log("fede");
+    console.log(sessionStorage);
+  };
 
-    const navigate = useNavigate();
+  return (
+    <div className="max-w-[900px] m-auto flex flex-col sm:flex-row sm:flex-wrap gap-1 sm:gap-2">
+      <div className="m-auto w-11/12 sticky top-0 py-2 bg-black">
+        <input
+          type="text"
+          placeholder="Buscar"
+          className="italic  px-3 py-2 bg-gray-800 border border-gray-600 rounded-lg shadow-sm shadow-gray-800  m-auto text-gray-400 outline-none w-20 focus:w-full focus:border-indigo-800 ease-in-out duration-300 ml-0"
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
+        ></input>
+      </div>
 
-    const setCoinToBeDetailed = (coin) => {
-        sessionStorage.setItem("token", JSON.stringify(coin));
-        navigate("/detailedCoin");
-        console.log('fede')
-        console.log(sessionStorage)
-      };
+      {showLoading && (
+        <div className="w-full h-screen flex ">
+          <ImSpinner9 className="m-auto text-4xl text-gray-600 animate-spin" />
+        </div>
+      )}
 
-    return (
-        <div className="max-w-[900px] m-auto flex flex-col sm:flex-row sm:flex-wrap gap-1 sm:gap-2">
+      {filteredData.map((e, index) => (
+        <div
+          /* onClick={()=>setCoinToBeDetailed(e)} */ key={index}
+          className="border w-11/12 sm:w-60 bg-gray-600/50 border-gray-600 rounded-lg shadow-sm shadow-gray-800 px-3 py-4 my-2 m-auto text-white relative"
+        >
+          <section className="flex flex-row gap-4 justify-left items-center w-full ">
+            <img
+              src={e.image}
+              alt="coin_img"
+              className="w-12 rounded-full shadow-lg shadow-black/50"
+            />
 
+            <div
+              className="font-semibold w-full"
+              onClick={() => setCoinToBeDetailed(e)}
+            >
+              <h1 className="text-white font-semibold">
+                {e.market_cap_rank}. <span className="text-xl">{e.name}</span>
+              </h1>
 
-            
-            
-            <div className="m-auto w-11/12 sticky top-0 py-2 bg-black">
-            <input type='text' placeholder="Buscar" className="italic  px-3 py-2 bg-gray-800 border border-gray-600 rounded-lg shadow-sm shadow-gray-800  m-auto text-gray-400 outline-none w-20 focus:w-full focus:border-indigo-800 ease-in-out duration-300 ml-0" onChange={(e)=>{setFilter(e.target.value)}}></input>
+              <div>Price: USD {e.current_price.toLocaleString("DE-de")}</div>
+
+              <div className="font-light">
+                Daily Change:
+                {e.price_change_percentage_24h < 0 ? (
+                  <span className="text-red-500 font-normal">
+                    {" "}
+                    {e.price_change_percentage_24h
+                      .toFixed(2)
+                      .toLocaleString("de-DE")}
+                    %
+                  </span>
+                ) : (
+                  <span className="text-green-700 font-normal">
+                    {" "}
+                    {e.price_change_percentage_24h
+                      .toFixed(2)
+                      .toLocaleString("de-DE")}
+                    %
+                  </span>
+                )}
+              </div>
             </div>
 
-            {showLoading && <div className="w-full h-screen flex "><ImSpinner9 className="m-auto text-4xl text-gray-600 animate-spin"/></div>}
-            
-    {filteredData.map((e, index)=>(
-                <div /* onClick={()=>setCoinToBeDetailed(e)} */ key={index} className="border w-11/12 sm:w-60 bg-gray-600/50 border-gray-600 rounded-lg shadow-sm shadow-gray-800 px-3 py-4 my-2 m-auto text-white relative">
-                    <section className="flex flex-row gap-4 justify-left items-center w-full ">
-                    <img src={e.image} alt="coin_img" className="w-12 rounded-full shadow-lg shadow-black/50"/>
-
-                    <div className="font-semibold w-full"  onClick={()=>setCoinToBeDetailed(e)}>
-                    
-                    <h1 className="text-white font-semibold">{e.market_cap_rank}. <span className="text-xl">{e.name}</span></h1>
-                    
-                    <div>Price: USD {e.current_price.toLocaleString('DE-de')
-                    }</div>
-                    
-                    
-                    <div className="font-light">Daily Change: 
-                    
-                     {e.price_change_percentage_24h<0 ? 
-                     <span className="text-red-500 font-normal"> {e.price_change_percentage_24h.toFixed(2).toLocaleString('de-DE')}%</span>
-                     : 
-                     <span className="text-green-700 font-normal"> {e.price_change_percentage_24h.toFixed(2).toLocaleString('de-DE')}%</span>}</div>
-                    </div>
-
-                    <div className="right-1 top-1 absolute z-20" >
-
-                    {
-                        favourites.includes(e.symbol) ?
-
-                        <AiFillHeart className="text-2xl text-red-500/50" onClick={()=>deleteFavToUpload(e.symbol)}/>
-
-                        :
-
-                        <AiOutlineHeart className="text-2xl text-gray-500" onClick={()=>addFavToUpload(e.symbol)}/>
-
-
-                    }
-
-                       
-                        
-                        
-                    </div>
-                    </section>
-
-
-                </div>
-            )) }
+            <div className="right-1 top-1 absolute z-20">
+              {favourites.includes(e.symbol) ? (
+                <Fade duration="500">
+                  <AiFillHeart
+                    className="text-2xl text-red-500/50 animate-spin-once"
+                    onClick={() => deleteFavToUpload(e.symbol)}
+                  />
+                </Fade>
+              ) : (
+                <Fade duration="500">
+                  <AiOutlineHeart
+                    className="text-2xl text-gray-500"
+                    onClick={() => addFavToUpload(e.symbol)}
+                  />
+                </Fade>
+              )}
+            </div>
+          </section>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default ShowCoins
+export default ShowCoins;
