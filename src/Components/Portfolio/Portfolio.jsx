5b44d5350c35a4d98bd6919ecf84490b7a3b181a
Fede <ferrelli.mazza@gmail.com>
2022-09-10 19:47:30 -0300

    Add chart to portafolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,11 +2,16 @@ import React, {useState, useEffect} from 'react';
 import db from '../../assets/firebase'
 import { collection, doc, setDoc, getDocs } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import PortfolioChart from '../../assets/Charts/PortfolioChart'
+//import Chart from "../../assets/Charts/Chart";
 
 function Portfolio({data}) {
-    console.log(data)
+    
     //const [portfolio, setPortfolio] = useState([])
     const [portfolioValue, setportfolioValue] = useState()
+    const [chartCoin, setChartCoin] = useState([]);
+    const [chartWeight, setChartWeight] = useState([]);
+    
  
     const portfolio = [{coinId:'bitcoin', qty : 0.0015}, 
                        {coinId:'ethereum', qty : 0.07}, 
@@ -48,10 +53,36 @@ useEffect(() => {
     
 }, [portfolio])
 
-console.log(portfolioValue)
+useEffect(() => {
+    
+    const auxChartCoin =[];
+    const auxChartWeight =[];
+    
+    portfolio.map(e=>{
+        auxChartCoin.push(e.name);
+        auxChartWeight.push(Number((e.totalValue/portfolioValue*100).toFixed(2)))
+    })
+
+    console.log(auxChartWeight)
+    setChartCoin(auxChartCoin);
+    setChartWeight(auxChartWeight)
+    
+}, [portfolioValue])
+
+console.log(chartWeight)
 
     return (
         <div>
+
+<section className="mb-8 pt-4 text-gray-600">
+      <h1 className="w-full mx-auto  text-4xl text-center ">Mi Portfolio</h1>    
+      {portfolioValue && <div className="w-full mx-auto text-3xl text-center font-bold mt-2">
+          USD {portfolioValue.toFixed(2).toLocaleString("DE-de")}
+        </div>  }
+        </section>
+<div className="w-11/12 m-auto">
+            <PortfolioChart coins={chartCoin} weights={chartWeight}/>
+            </div>
             <div className="text-gray-600 flex pl-4"><button className="text-xl m-auto" onClick={()=>navigate('/')}>Lista</button></div>
 
 
---
+++ src/assets/Charts/PortfolioChart.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export function PortfolioChart({coins, weights}) {
+    const data = weights;
+    const labels = coins;
+
+    console.log(weights) 
+  return <Doughnut data={{
+    labels: labels,
+    datasets: [
+      {
+        label: '# of Votes',
+        data: data,
+        backgroundColor: coins.map(e=>
+            `rgba(${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)})`
+            ),
+        borderWidth: 1,
+      },
+    ],
+  }} />;
+}
+
+
+export default PortfolioChart
